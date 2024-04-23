caf0ee77cc4a94de8104ddf39c62fabaea92a737
AlexPazCodesUCSD <146804535+AlexPazCodesUCSD@users.noreply.github.com>
2024-04-22 23:59:09 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,14 @@ led_pins = [
     board.IO21,
     board.IO26, # type: ignore
     board.IO47,
-    # do the rest...
+    board.IO33,
+    board.IO34,
+    board.IO48,
+    board.IO35,
+    board.IO36,
+    board.IO37,
+    board.IO38,
+    board.IO39
 ]
 
 leds = [DigitalInOut(pin) for pin in led_pins]
@@ -24,13 +31,160 @@ for led in leds:
 # main loop
 while True:
     volume = microphone.value
+    for x in leds:
+        x.value = 0;
+    
 
     print(volume)
+    num_lights = len(leds)
+    
+    if volume < 5000:
+        leds[0].value = 1;
+        leds[1].value = 0;
+        leds[2].value = 0;
+        leds[3].value = 0;
+        leds[4].value = 0;
+        leds[5].value = 0;
+        leds[6].value = 0;
+        leds[7].value = 0;
+        leds[8].value = 0;
+        leds[9].value = 0;
+        leds[10].value = 0;
+    
+    elif volume < 10000:
+        leds[0].value = 1;
+        leds[1].value = 1;
+        leds[2].value = 0;
+        leds[3].value = 0;
+        leds[4].value = 0;
+        leds[5].value = 0;
+        leds[6].value = 0;
+        leds[7].value = 0;
+        leds[8].value = 0;
+        leds[9].value = 0;
+        leds[10].value = 0;
+    
+    elif volume < 15000:
+        leds[0].value = 1;
+        leds[1].value = 1;
+        leds[2].value = 1;
+        leds[3].value = 0;
+        leds[4].value = 0;
+        leds[5].value = 0;
+        leds[6].value = 0;
+        leds[7].value = 0;
+        leds[8].value = 0;
+        leds[9].value = 0;
+        leds[10].value = 0;
+    
+    elif volume < 20000:
+        leds[0].value = 1;
+        leds[1].value = 1;
+        leds[2].value = 1;
+        leds[3].value = 1;
+        leds[4].value = 0;
+        leds[5].value = 0;
+        leds[6].value = 0;
+        leds[7].value = 0;
+        leds[8].value = 0;
+        leds[9].value = 0;
+        leds[10].value = 0;
+    
+    elif volume < 25000:
+        leds[0].value = 1;
+        leds[1].value = 1;
+        leds[2].value = 1;
+        leds[3].value = 1;
+        leds[4].value = 1;
+        leds[5].value = 0;
+        leds[6].value = 0;
+        leds[7].value = 0;
+        leds[8].value = 0;
+        leds[9].value = 0;
+        leds[10].value = 0;
+    
+    elif volume < 30000:
+        leds[0].value = 1;
+        leds[1].value = 1;
+        leds[2].value = 1;
+        leds[3].value = 1;
+        leds[4].value = 1;
+        leds[5].value = 1;
+        leds[6].value = 0;
+        leds[7].value = 0;
+        leds[8].value = 0;
+        leds[9].value = 0;
+        leds[10].value = 0;
+    
+    elif volume < 35000:
+        leds[0].value = 1;
+        leds[1].value = 1;
+        leds[2].value = 1;
+        leds[3].value = 1;
+        leds[4].value = 1;
+        leds[5].value = 1;
+        leds[6].value = 1;
+        leds[7].value = 0;
+        leds[8].value = 0;
+        leds[9].value = 0;
+        leds[10].value = 0;
+        
+    
+    elif volume < 40000:
+        leds[0].value = 1;
+        leds[1].value = 1;
+        leds[2].value = 1;
+        leds[3].value = 1;
+        leds[4].value = 1;
+        leds[5].value = 1;
+        leds[6].value = 1;
+        leds[7].value = 1;
+        leds[8].value = 0;
+        leds[9].value = 0;
+        leds[10].value = 0;
+    
+    elif volume < 45000:
+        leds[0].value = 1;
+        leds[1].value = 1;
+        leds[2].value = 1;
+        leds[3].value = 1;
+        leds[4].value = 1;
+        leds[5].value = 1;
+        leds[6].value = 1;
+        leds[7].value = 1;
+        leds[8].value = 1;
+        leds[9].value = 0;
+        leds[10].value = 0;
+    
+    elif volume < 50000:
+        leds[0].value = 1;
+        leds[1].value = 1;
+        leds[2].value = 1;
+        leds[3].value = 1;
+        leds[4].value = 1;
+        leds[5].value = 1;
+        leds[6].value = 1;
+        leds[7].value = 1;
+        leds[8].value = 1;
+        leds[9].value = 1;
+        leds[10].value = 0;
+    
+    else :
+        leds[0].value = 1;
+        leds[1].value = 0;
+        leds[2].value = 1;
+        leds[3].value = 1;
+        leds[4].value = 1;
+        leds[5].value = 1;
+        leds[6].value = 1;
+        leds[7].value = 1;
+        leds[8].value = 1;
+        leds[9].value = 1;
+        leds[10].value = 0;
 
-    leds[0].value = not leds[0].value
-    leds[1].value = not leds[0].value
 
-    sleep(1)
+
+    sleep(0.05)
 
     # instead of blinking,
     # how can you make the LEDs
